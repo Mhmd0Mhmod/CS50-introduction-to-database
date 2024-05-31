d4a1b57edb842cb099c5eea7950d3b8c608e4563
Mohamed Mahmoud <112190828+Mhmd0Mhmod@users.noreply.github.com>
2024-05-31 15:25:01 +0300

    init

---
+++ Relating/Relating.sql
@@ -0,0 +1,5 @@
+-- database: d:\MySelf Learn\Backend\Database\CS50-introduction-to-database\longlist.db
+
+-- Use the ▷ button in the top right corner to run the entire file.
+
+SELECT * FROM longlist;
